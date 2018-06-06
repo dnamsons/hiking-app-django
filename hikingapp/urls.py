5dbd423a817a3c00e4ab7d51194e21af5f66476e
jimkijs <davisnamsons@gmail.com>
2018-06-06 21:31:08 +0300

    registration

--- hikingapp/urls.py
+++ hikingapp/urls.py
@@ -18,9 +18,11 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
+from registration import views as registration_views
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^signup/$', registration_views.signup, name='signup'),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
     url(r'^admin/', admin.site.urls),
