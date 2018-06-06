7514c3206d5b1ac0b929593d837d8f111722391b
Jānis Kristaps Lūsis <jklusis@github.com>
2018-06-06 23:28:16 +0300

    static > staticfiles

--- hikingapp/settings.py
+++ hikingapp/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),  # Global static file location is 'staticfiles'
+    os.path.join(BASE_DIR, "static"),  # Global static file location is 'staticfiles'
 ]
 
 LOGIN_REDIRECT_URL = 'home'
