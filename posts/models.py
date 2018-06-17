ae4740c18f372f0813f4c12e09e44bcce4da324f
dnamsons <davisnamsons@gmail.com>
2018-06-17 20:45:39 +0300

    created post models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class UserPost(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    post_author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_author')
+    image = models.ImageField(upload_to=get_image_path, blank=True, null=True, default='images/no_image.jpg')
+    post = models.CharField(max_length=300, default='', blank=True)
+    like_amount = models.IntegerField(default=0)
+    comment_amount = models.IntegerField(default=0)
+
+class UserComment(models.Model):
+    post_id = models.ForeignKey(UserPost, on_delete=models.CASCADE, related_name='%(class)s_author')
+    comment_author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_author')
+    comment = models.CharField(max_length=200, default='', blank=True)
+
+class UserLike(models.Model):
+    post_id = models.ForeignKey(UserPost, on_delete=models.CASCADE, related_name='%(class)s_author')
+    like_author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_author')
